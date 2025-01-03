04a42f2dfade7adec92e410ef540a86ba4927bbb
shivani sharma <sharmashivani0619@gmail.com>
2025-01-04 01:21:42 +0530

    updated mongo uri string

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-    await mongoose.connect('mongodb://localhost:27017/dev-tinder')
+    //await mongoose.connect('mongodb://localhost:27017/dev-tinder')
+    await mongoose.connect('mongodb+srv://tinderDev:tinderDev@tinderdev.jc6eg.mongodb.net/tinderDev')
 };
-
 module.exports = connectDB;
